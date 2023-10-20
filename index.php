1fa0eff14026ac0bffc2c55563c3534471291258
Mansi Verma <99127354+Vmansi115@users.noreply.github.com>
2023-10-20 18:30:27 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>BANKING SYSTEM</title>
+  <link rel="stylesheet" href="style_ind.css"></link>
+  </head>
+  <body style="background-color: whitesmoke;">
+<div class="navbar">
+<center>
+  <hr color="black" width=70% size=1>
+  <h1> SECUREVAULT</h1>
+  <a class="active" href="index.php">Home</a>
+  <a href="sendmoney.php">Transfer Money</a>
+  <a href="customers.php">View All Customers</a>
+  <a href="transactions.php">Transactions</a>
+  <a href="contact.php">Contact Us</a>
+  <a href="about.php">About Us</a>
+  <hr color="grey" width=70% size=1 align>
+  </center>
+</div>
+<div class="container">
+<div class="header">Welcome To Securevault!
+</div>
+<img src="https://graphicsfamily.com/wp-content/uploads/2020/05/3D-Logo-Mockup-4-scaled.jpg" height=80% width=20% 
+alt="Welcome to the Securevault!" style="padding: 3px; margin-right: 6vw; margin-top:1vh; float:right"> 
+</div>
+<center>
+<marquee> IN BANKING OR FINANCE, TRUST IS THE ONLY THING YOU HAVE TO SELL.  IN BANKING OR FINANCE, TRUST IS THE ONLY THING YOU HAVE TO SELL.  IN BANKING OR FINANCE, TRUST IS THE ONLY THING YOU HAVE TO SELL.  IN BANKING OR FINANCE, TRUST IS THE ONLY THING YOU HAVE TO SELL.</marquee>
+</class>
+<main>
+  <div class="image">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTawNGkug5oftXFSCVnfAr_flcpvCFBUwg0fw&usqp=CAU">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuaHejpLfniRg36oDFFvw0EJ5f-adKbMYkBA&usqp=CAU">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE1EKCDG2iiqxfWY0MxCb2NFC3Kx8K16SvHQ&usqp=CAU">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5nyuq7BPtVcLku_WE6dSwaH0-CzlduksF-ha9RZEtYyC5qwyZWuJznjow0jfUHMqxdQA&usqp=CAU">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8sC1Fdn6TdS8yateanNO6jUbuXwFnzTQ-hQ&usqp=CAU">
+  </div>
+</main>
+<div class="contentbox"> 
+<br>
+<a class="button" href="sendmoney.php">TRANSFER MONEY</a>
+<br><br><br><br>
+<a class="button" href="customers.php">VIEW ALL CUSTOMERS</a>
+<br><br><br><br>
+<a class="button" href="transactions.php">TRANSACTION HISTORY</a>
+</div>
+<div class="pagebreak">
+</div>
+<div class="contentbox">
+  <h1> GUIDELINES</h1>
+      <ul><h4>
+        <li>Customers are requested to get their kyc done.</li>
+        <li>Aadhar card is a mandatory document for opening a bank account.</li>
+        <li>The bank never calls you and ask for any OTP. Please be cautious.</li>
+        <li>Customers are advised not to to share their online banking credentials.</li>
+      </ul> </h4>
+      <br>
+  </div>
+<div class="pagebreak">
+</div>
+<br>
+<marquee> PLEASE BEWARE OF FRAUDS!PLEASE BEWARE OF FRAUDS!PLEASE BEWARE OF FRAUDS!</marquee>
+<div class="footer">
+ <center>
+  <p>@ 2023 Copyright-Made by Mansi Verma, Intern at The Sparks Foundation.All rights reserved.</p>
+  <a href= "terms.php"> Terms & Conditions</a>
+  <a href ="privacy.php">Privacy Policy</a>
+</center>
+</div></body>
+</html>
