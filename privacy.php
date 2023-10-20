9a7f5fbdf644b1ccb85077bd461970d120bfff50
Mansi Verma <99127354+Vmansi115@users.noreply.github.com>
2023-10-20 18:31:06 +0530

    Create privacy.php

---
+++ privacy.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>BANKING SYSTEM</title>
+    <link rel="stylesheet" href="style_about.css">
+  </head>
+  <body style="background-color: whitesmoke;">
+<div class="navbar">
+<center>
+    <hr color="black" width=70% size=1>
+  <h1> SECUREVAULT </h1>
+  <a  href="index.php">Home</a>
+  <a href="sendmoney.php">Transfer Money</a>
+  <a href="customers.php">View All Customers</a>
+  <a href="transactions.php">Transactions</a>
+  <a href="contact.php">Contact Us</a>
+  <a class="active" href="about.php">About Us</a>
+     <hr color="black" width=70% size=1 align>
+      </center>
+</div>
+<center>
+<div class="contentbox">
+  <h1> SECUREVAULT</h1>
+  <br>
+  <h3> PRIVACY POLICY</h3>
+  <h5> We are bound by the Acts and Regulations relating to Privacy and will protect your non-public
+     personal information against disclosure, misuse, reuse or use in unrestricted manner in
+      accordance with the law. Respecting and maintaining your online privacy and the security of 
+      your personal information is extremely important to us. These principles govern how we 
+      can collect, use, hold and disclose your personal information.Securevault PRIVACY POLICY is designed to inform you
+      of our online and offline information privacy practices, the type of information we collect, 
+      how the information is used, and to assure you that e maintain strict security procedures to protect 
+      your information.<br><br>
+    <h4> What type of information?</h4>
+    <br>
+    Information can be broadly categorised as publicly available information and non-public (both personal and finanacial) information.
+    <br>
+    Non-public information is covered under this privacy policy.
+    <br>
+    Publicly available information is any information Bank reasonable believes is lawfully publicly available.
+    The nature of the information, not the source of the information,determines whether it is publicly available information for 
+    purposes of the privacy policy.
+    <br>
+    <br>
+      <h4> To whom it applies?</h4>
+      <br>
+      It applies to all our customers, account holders, consumers, clients, service providers, contractors, sub-contractors, affiliates, and their clients.
+  <div class="footer">
+ <center>
+  <p>@ 2023 Copyright-Made by Mansi Verma, Intern at The Sparks Foundation.All rights reserved.</p>
+  <a href= "terms.php"> Terms & Conditions</a>
+  <a href ="privacy.php">Privacy Policy</a>
+</center>
+</div>
+</body>
+</html>
