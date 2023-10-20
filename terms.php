e9feebdc4662dfb5583162e66463e943d22a07d3
Mansi Verma <99127354+Vmansi115@users.noreply.github.com>
2023-10-20 18:48:43 +0530

    Create terms.php

---
+++ terms.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>BANKING SYSTEM</title>
+<link rel="stylesheet" href="style_about.css">
+  </head>
+  <body style="background-color: whitesmoke;">
+<div class="navbar">
+<center>
+    <hr color="black" width=70% size=1>
+  <h1> SECUREVAULT </h1>
+  <a  href="index.php">Home</a>
+  <a href="sendmoney.php">Transfer Money</a>
+  <a href="customers.php">View All Customers</a>
+  <a href="transactions.php">Transactions</a>
+  <a href="contact.php">Contact Us</a>
+  <a class="active" href="about.php">About Us</a>
+     <hr color="black" width=70% size=1 align>
+      </center>
+</div>
+<div class="container">
+<div class="header" > Welcome to Securevault! </div>
+<img src="https://graphicsfamily.com/wp-content/uploads/2020/05/3D-Logo-Mockup-4-scaled.jpg" height=80% width=20% 
+alt="Welcome to the Securevault!" style="padding: 5px; margin-right: 8vw; margin-top:1vh; float:right"> 
+</div>
+<marquee> IN BANKING OR FINANCE, TRUST IS THE ONLY THING YOU HAVE TO SELL.  IN BANKING OR FINANCE, TRUST IS THE ONLY THING YOU HAVE TO SELL.  IN BANKING OR FINANCE, TRUST IS THE ONLY THING YOU HAVE TO SELL.  IN BANKING OR FINANCE, TRUST IS THE ONLY THING YOU HAVE TO SELL.</marquee>
+ <center>
+<div class="contentbox">
+    <h3>Definitions</h3>
+    <h5>In this document the following words and phrases shall have the meanings as set below 
+    unless the context indicates otherwise:</h5>
+    <br>
+    <h4> "Account(s)"</h4>
+    <h5> refers to the Customer's Savings/Current Account and/ or home loan account and/ or
+         automobile loan account and/ or consumer durable loan account and/ or any other type of 
+        account so maintained with Securevault which are eligible Account(s) for operations through
+         the use of Securevault Mobile(Core Banking) Services (each an "Account"
+        and collectively "Accounts").</h5>
+    <h4>“Alerts”</h4>
+    <h5> refers to the event-based message sent to the User, on his mobile phone or his email 
+        address subject to the User’s subscribing to that event-based message.</h5>
+    <br>
+    <h4>"Bank”</h4>
+    <h5> means Bank of India, a body corporate constituted under the Banking Companies (Acquisition
+        and Transfer of Undertakings) Act, 1970.</h5>
+    <br>
+    <h4>“Customer”</h4> 
+    <h5> means a person including individual(s), Company, Proprietary Firm, HUF, etc…who has an 
+        Account with the Bank and who has been authorized by the Bank to avail of the Securevault 
+        Mobile (Core Banking)  Services interalia on the terms and conditions contained herein.</h5>
+        <br>
+    <h4> “IVRS”</h4>
+    <h5>means Interactive Voice Response System used on telephone systems to convert data into voice
+         and send telephone keypad signals to computer systems to select the choices.</h5>
+    <br>
+    <h4>“Password” </h4>
+    <h5>is a secret series of characters comprising of letters of English alphabet and/or numeric
+         numbers and/or special characters that enables a computer User to access a computer network,
+         file, data or programme.</h5>
+    </br>
+    <h4>"Personal information"</h4>
+    <h5> refers to the information provided by the Customer/User to the Bank.</h5>
+    <br>
+    <h4> "SMS Banking"</h4>
+    <h5> shall mean the Bank's SMS banking facility under Securevault Mobile (Core Banking) 
+       Services which provides the Customer services such as information relating to Account(s) of 
+       the Customer, details about transactions, utility payment, funds transfer and such other 
+       services as may be provided on the Customer's Mobile Telephone using ‘Short Messaging Service’ 
+       (SMS) by the Bank from time to time.</h5>
+    <br>
+   <h4>“SMS Password”</h4>
+   <h5> is a secret series of numeric numbers that enables a User to access Securevault Mobile
+     (Core Banking) Services using SMS Banking.</h5>
+    <br>
+    <h4>“Securevault Mobile (Core Banking) Services” </h4>
+    <h5>is the Bank’s Services extended to its Core Banking Branch Customers through Mobile Phones i.e. Mobile Banking, SMS Banking, like:
+         <ol>
+        <li>Account Status; which includes balance enquiry, transactional view, statement of accounts, etc.</li>
+        <li>Services/ Requests; like Request for Cheque book, Stop Payment, Utility Payments, deposit renewal etc.</li>
+        <li> Financial Transactions; which include transfer of funds (to self-account and/or credit/debit to third 
+          party account, IMPS P2P,IMPS P2M, NEFT,RTGS, Bill payments, term deposit Transactions)etc.</li>
+        <li> Administration Module;</li> 
+        <li>Event Based Alerts;</li>
+        <li>Utilities; would include display of, deposit interest rates, etc.</li>
+        </ol>
+        These facilities shall be offered in a phased manner at the convenience of the Bank. 
+        The Bank may also make additions/ modifications/ deletions to the facilities offered through 
+        Securevault Mobile (Core Banking) Services, at its sole discretion without any intimation to customer/user.</h5><br>
+    <h4> "Terms"</h4>
+    <h5> refers to the terms and conditions for use of Securevault Mobile (Core Banking) Services 
+        as detailed in this document.</h5>
+        <br>
+    <h4>"TPIN"</h4>
+    <h5> refers to the Telebanking Personal Identification Number and is a unique number, that is
+         required for the accessing the facility and will be useful for general enquiry and 
+         requests, over Telephone.</h5>
+    <br>
+    <h4> “TELEBANKING”</h4>
+    <h5> means the facility which may be provided by the Bank to the Customers for getting
+         information through IVRS about the Customer’s Account/s using telephone
+   with key pad facility and/or for registering for any services as may be offered by the Bank under
+    tele-banking facility.</h5>
+    <br>
+    <h4>"Transaction PIN"</h4>
+    <h5> refers to the Transaction Personal Identification Number and is a unique number that is
+         required for executing the transactions over Telephone.</h5>
+    <br>
+    <h4>"User"</h4>
+    <h5> refers to the person(s) authorized to use Securevault Mobile (Core Banking) Services by the Bank,
+      on the request of its Customer.
+        <ol>
+        <li>In case of Customer being the Hindu Undivided Family (HUF), 
+      the Karta of the HUF shall be authorized to be use Securevault Mobile (Core Banking) Services and the 
+      same shall bind all the members of the HUF.</li>
+        <li> In case of customer being a company/firm/other bodies, the person authorized to use the
+ Securevault Mobile( Core Banking) Services and the same 
+       shall be binding on the company/firm/ other bodies* In case the Customer is an Individual 
+       the individual himself/herself.</li>
+        </ol>
+    </h5>
+    <br>
+    <h4>“User-ID”</h4>
+    <h5> refers to short collection of characters and/or 
+       numerals to be used to identify the User who is using the password.In this document all 
+       references to the User in masculine gender shall be deemed to include the feminine gender and
+        vice versa.</h5>
+
+<div class="footer">
+ <center>
+  <p>@ 2023 Copyright-Made by Mansi Verma, Intern at The Sparks Foundation.All rights reserved.</p>
+  <a href= "terms.php"> Terms & Conditions</a>
+  <a href ="privacy.php">Privacy Policy</a>
+</center>
+</div>
+</body>
+</html>
