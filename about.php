9daf6193049d14fcaeae4e728aedb66ba6bf74a7
Mansi Verma <99127354+Vmansi115@users.noreply.github.com>
2023-10-20 18:27:03 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>BANKING SYSTEM</title>
+<link rel="stylesheet" href="style_about.css">
+  </head>
+  <body style="background-color: whitesmoke;">
+<div class="navbar">
+<center>
+    <hr color="black" width=70% size=1>
+  <h1> SECUREVAULT </h1>
+  <a  href="index.php">Home</a>
+  <a href="sendmoney.php">Transfer Money</a>
+  <a href="customers.php">View All Customers</a>
+  <a href="transactions.php">Transactions</a>
+  <a href="contact.php">Contact Us</a>
+  <a class="active" href="about.php">About Us</a>
+     <hr color="black" width=70% size=1 align>
+      </center>
+</div>
+<div class="container">
+<div class="header" > Welcome to Securevault! </div>
+<img src="https://graphicsfamily.com/wp-content/uploads/2020/05/3D-Logo-Mockup-4-scaled.jpg" height=80% width=20% 
+alt="Welcome to the Securevault!" style="padding: 5px; margin-right: 8vw; margin-top:1vh; float:right"> 
+</div>
+<marquee> IN BANKING OR FINANCE, TRUST IS THE ONLY THING YOU HAVE TO SELL.  IN BANKING OR FINANCE, TRUST IS THE ONLY THING YOU HAVE TO SELL.  IN BANKING OR FINANCE, TRUST IS THE ONLY THING YOU HAVE TO SELL.  IN BANKING OR FINANCE, TRUST IS THE ONLY THING YOU HAVE TO SELL.</marquee>
+ <center>
+<div class="contentbox">
+  <h1> ABOUT US</h1>
+  <h5> This website is created by Mansi Verma from Guru Tegh Bahadur Institute Of Technology, currently doing internship with The Sparks Foundation." 
+  </h5>
+  <h2> THE SPARKS FOUNDATION</h2>
+  <h5> The Sparks Foundation is working to bring parity in education, making sure children have equal opportunity at success, irrespective of the financial background.</h5>
+  <br>
+  <br>
+<a class="button" href="https://www.thesparksfoundationsingapore.org/"> 
+  Know more about us
+</a>
+</div>
+</center>
+<div class="pagebreak">
+</div>
+<br>
+<marquee> PLEASE BEWARE OF FRAUDS!PLEASE BEWARE OF FRAUDS!PLEASE BEWARE OF FRAUDS!</marquee>
+<div class="footer">
+ <center>
+  <p>@ 2023 Copyright-Made by Mansi Verma, Intern at The Sparks Foundation.All rights reserved.</p>
+  <a href= "terms.php"> Terms & Conditions</a>
+  <a href ="privacy.php">Privacy Policy</a>
+</center>
+</div>
+</body>
+</html>
